cfd0c14edbd76fcf77c6ce46c67792f4e6cefa6b
Yuu <yy556023@gmail.com>
2023-12-09 16:57:25 +0800

    初階功能完成

--- SpecGenerator.cs
+++ SpecGenerator.cs
@@ -31,11 +31,18 @@ namespace SpecGenerator
             MenuListBox.DataSource = TotalMenu.ChildItems;
             MenuListBox.DisplayMember = "ItemName";
             MenuNameTextBox.Text = string.Empty;
+            MenuListBox.ClearSelected();
         }
 
         private void PageAddButton_Click(object sender, EventArgs e)
         {
             var pageName = PageNameTextBox.Text;
+
+            if (MenuListBox.SelectedItem == null)
+            {
+                MessageBox.Show("請先點選Menu", "Menu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
             if (string.IsNullOrEmpty(pageName))
             {
@@ -55,19 +62,37 @@ namespace SpecGenerator
             PageListBox.DataSource = selectedItem.ChildItems;
             PageListBox.DisplayMember = "ItemName";
             PageNameTextBox.Text = string.Empty;
+            PageListBox.ClearSelected();
         }
 
         private void ActionAddButton_Click(object sender, EventArgs e)
         {
             var actionName = ActionNameTextBox.Text;
 
+            if (PageListBox.SelectedItem == null)
+            {
+                MessageBox.Show("請先點選Page", "Page", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             if (string.IsNullOrEmpty(actionName))
             {
                 return;
             }
 
-            ActionListBox.Items.Add(actionName);
+            var item = new MenuItem()
+            {
+                ItemName = actionName
+            };
+
+            var selectedItem = (MenuItem)PageListBox.SelectedItem;
+
+            selectedItem.ChildItems.Add(item);
+            ActionListBox.DataSource = null;
+            ActionListBox.DataSource = selectedItem.ChildItems;
+            ActionListBox.DisplayMember = "ItemName";
             ActionNameTextBox.Text = string.Empty;
+            ActionListBox.ClearSelected();
         }
 
         private void MenuRemoveButton_Click(object sender, EventArgs e)
@@ -104,7 +129,24 @@ namespace SpecGenerator
 
         private void ActionRemoveButton_Click(object sender, EventArgs e)
         {
+            if (PageListBox.SelectedItem == null)
+            {
+                return;
+            }
 
+            if (ActionListBox.SelectedItem == null)
+            {
+                return;
+            }
+
+            var pageItem = (MenuItem)PageListBox.SelectedItem;
+            var actionItem = (MenuItem)ActionListBox.SelectedItem;
+
+            pageItem.ChildItems.Remove(actionItem);
+            ActionListBox.DataSource = null;
+            ActionListBox.DataSource = pageItem.ChildItems;
+            ActionListBox.DisplayMember = "ItemName";
+            ActionListBox.ClearSelected();
         }
 
         private void TxtButton_Click(object sender, EventArgs e)
@@ -114,11 +156,16 @@ namespace SpecGenerator
             foreach (var menu in TotalMenu.ChildItems)
             {
                 // ┖ ┕ └ ─ ━
-                sb.AppendLine($"━━━{menu.ItemName}");
+                sb.AppendLine($"╠═ {menu.ItemName}");
 
                 foreach (var page in menu.ChildItems)
                 {
-                    sb.AppendLine($"┕━━━{page.ItemName}");
+                    sb.AppendLine($"║    ╠═ {page.ItemName}");
+
+                    foreach (var action in page.ChildItems)
+                    {
+                        sb.AppendLine($"║          ╠═ {action.ItemName}");
+                    }
                 }
             }
 
